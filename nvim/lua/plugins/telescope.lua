887f5352d6c1b07d692a7b24d8909d7f577fdaa8
changliu0828 <changliu0828@gmail.com>
2023-07-18 16:25:51 +0900

    udpate

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,13 +8,6 @@ require('telescope').setup{
     }
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
   },
   extensions = {
     -- Your extension configuration goes here:
